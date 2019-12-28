077854b77ce5c6276795a1e5a01f3be3800ad7ff
Christoph Ehlers <Christoph.Ehlers1988@gmx.de>
2019-12-28 22:59:13 +0100

    basic stuff

--- src/serverplugin/AuthDbPlugin.ts
+++ src/serverplugin/AuthDbPlugin.ts
@@ -2,12 +2,12 @@ import Serverplugin from "socommujs/dist/core/Serverplugin";
 import {serverevent} from "socommujs/dist/core/enums";
 
 export default class extends Serverplugin {
+    constructor() {
+        super();
+        this.setName('AuthDbPlugin');
+    }
     getListenEvents(): serverevent[] {
         return [];
-    }
-
-    getName(): string {
-        return "";
     }
 
     handleEvent(event: serverevent, eventProps?: unknown): void {
--- src/serverplugin/TerminalPlugin.ts
+++ src/serverplugin/TerminalPlugin.ts
@@ -1,16 +1,17 @@
-import Serverplugin from "socommujs/src/core/Serverplugin";
-import {serverevent} from "socommujs/src/core/enums";
-import {getServereventString} from "socommujs/src/core/utils";
-import Engine from "terminal-scene/src/Engine";
-import Eventhandler from "socommujs/src/Eventhandler";
-import DebugScene from "terminal-scene/src/Scenes/DebugScene";
-import PluginlistScene from "terminal-scene/src/Scenes/PluginlistScene";
+import Serverplugin from "socommujs/dist/core/Serverplugin";
+import {serverevent} from "socommujs/dist/core/enums";
+import {getServereventString} from "socommujs/dist/core/utils";
+import Engine from "terminal-scene/dist/Engine";
 
 export default class extends Serverplugin {
     protected TermEngine:Engine=new Engine();
     private updateHandle:any=null;
     private updateInterval:number=500;
 
+    constructor() {
+        super();
+        this.setName('TerminalPlugin')
+    }
     getListenEvents(): serverevent[] {
         return [
             serverevent.clientWillConnect
@@ -28,10 +29,5 @@ export default class extends Serverplugin {
     }
     run(data?: unknown): void {
 
-    }/*
-    init(props: { EventHandler:Eventhandler }): Promise<unknown> {
-        this.TermEngine.addScene(new DebugScene(10,20,50,10));
-        this.TermEngine.addScene(new PluginlistScene(1,1,23,10));
-        return super.init(props);
-    }*/
+    }
 }
